0c81bfbe57e3a5480cd95ffb41353427616ae13b
CatalinVoss <catalin@cs.stanford.edu>
2020-01-04 17:50:38 -0800

    Install exit button and robustify clear button hack on iOS

--- ios/RNZendesk/FrameNavigationController.swift
+++ ios/RNZendesk/FrameNavigationController.swift
@@ -13,11 +13,33 @@
 import Foundation
 
 class FrameNavigationController: UINavigationController {
-    func clearLeftButton() {
+    var exitCallback:RCTResponseSenderBlock? = nil
+    
+    @objc func exit(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Sign Out", message: "Do you want to sign out?", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { action in
+            if self.exitCallback != nil {
+                self.dismiss(animated: true, completion: nil)
+                self.exitCallback?([NSNull(), NSNull()])
+            }
+        }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func performClear() {
         if (self.viewControllers.count == 1) {
-            let emptyButton = UIBarButtonItem(title: "", style: UIBarButtonItem.Style.plain, target: nil, action: nil)
-            self.navigationBar.topItem?.leftBarButtonItem = emptyButton
-            self.visibleViewController?.navigationItem.leftBarButtonItem = emptyButton
+            let exitButton = UIBarButtonItem(image: UIImage(named: "logout"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(exit(sender:)))
+            self.navigationBar.topItem?.leftBarButtonItem = exitButton
+            self.visibleViewController?.navigationItem.leftBarButtonItem = exitButton
+        }
+    }
+
+    func clearLeftButton() {
+        // Now and again in 0.5 seconds to be fucking safe
+        self.performClear()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.performClear()
         }
     }
         
